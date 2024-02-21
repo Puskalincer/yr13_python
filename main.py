a8affd0b5a6948316aa58db0a4cefd84447f8f02
Jullian <jullian.bublitz@wbhs.school.nz>
2024-02-21 15:00:43 +1300

    v0.17.2.1

--- main.py
+++ main.py
@@ -7,6 +7,26 @@ import os
 import html
 
 """
+LOOK AT THIS LATERRR
+
+import SimpleHTTPServer
+import SocketServer
+
+class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/your_file.html'
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+Handler = MyRequestHandler
+server = SocketServer.TCPServer(('0.0.0.0', 8080), Handler)
+
+server.serve_forever()
+"""
+
+
+
+"""
 TOO DO LIST
 
 Add error checking to everything.
